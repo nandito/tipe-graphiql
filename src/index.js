4e47b0c98ed12e42041c3c7ac0a7793d458c8e30
Nandor Biro <nandor.biro@digitalnatives.hu>
2018-02-03 17:27:49 +0100

    Replace sample React code with GraphiQL

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import GraphiQL from 'graphiql';
+import fetch from 'isomorphic-fetch';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import '../node_modules/graphiql/graphiql.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const API_KEY = 'your_api_key';
+const ORG_SECRET_KEY = 'your_org_secret';
+
+function graphQLFetcher(graphQLParams) {
+  return fetch('https://api.tipe.io/graphql', {
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': API_KEY,
+      'Tipe-Id': ORG_SECRET_KEY,
+    },
+    body: JSON.stringify(graphQLParams),
+  }).then(response => response.json());
+}
+
+ReactDOM.render(<GraphiQL fetcher={graphQLFetcher} />, document.getElementById('root'));
